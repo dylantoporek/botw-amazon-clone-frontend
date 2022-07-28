945b77be877b4f1b13fcf513b45169e8e304001c
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-07-28 14:21:18 -0400

    tailwind removed, scss added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './input.css';
+import Navbar from './Components/Navbar/Navbar'
 
 
 function App() {
   return (
     <div>
-
-      Hi from app
-
+      <Navbar/>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,1 +1,12 @@
+import React from "react";
+import './index.scss'
 
+function Navbar(){
+    return (
+        <div className="navbar-container">
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
