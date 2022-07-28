41bf515f984ddd0737070501ebcdefef8fa45af4
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-07-28 13:21:32 -0400

    restart

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './input.css';
-import Navbar from './Navbar/Navbar.js'
+
 
 function App() {
   return (
     <div>
-      <div>
-        <Navbar/>
-      </div>
+
+      Hi from app
+
     </div>
   );
 }
