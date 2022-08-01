6b7dc122eab25181bfb10ade33f45ae521949db7
RaccoonNW <87783832+RaccoonNW@users.noreply.github.com>
2022-08-01 16:19:46 -0600

    Add fetch for all weapons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './input.css';
@@ -6,6 +6,18 @@ import Navbar from './Components/Navbar/Navbar'
 
 
 function App() {
+
+  const [weapons, setWeapons] = useState({})
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:3000/weapons/index')
+      .then((r) => r.json())
+      .then(weapons => setWeapons(weapons))
+
+  }, [])
+
+  console.log(weapons)
+
   return (
     <div>
       <Navbar/>
